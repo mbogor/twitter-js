b5cea36f8d7514b6f090d5adfc6988a1c7631489
Ahertig <alyssa.hertig@gmail.com>
2016-01-20 18:59:26 -0500

    Front page is now displaying 10 tweets

--- app.js
+++ app.js
@@ -2,20 +2,42 @@ var express = require('express');
 var app = express(); //instance of express
 var swig = require('swig');
 
+var routes = require('./routes/');
+
+app.use('/', routes);
+
+// app.use(express.static('public'));
+
+// app.get("/", function (req, res) {
+//   res.sendFile(__dirname + '/routes/index.js');
+// });
+
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + "/views");
+
 app.listen(3000, function(){
-	console.log("server listening");
+  console.log("server listening");
 });
 
-app.get("/", function (req, res) {
-  var people = [{name: 'Grace Hopper'}, {name: 'Alyssa'}, {name: 'Mariya'}];
-  res.render('index', {title: 'Grasshoppers', people: people});
+swig.setDefaults({ cache: false});
 
-});
 
-app.get("/special", function (req, res) {
-  res.send("GET / " +res.statusCode+"\n");
-  //next();
-});
+
+
+
+
+
+// app.get("/", function (req, res) {
+//   var people = [{name: 'Grace Hopper'}, {name: 'Alyssa'}, {name: 'Mariya'}];
+//   res.render('index', {title: 'Grasshoppers', people: people});
+
+// });
+
+// app.get("/special", function (req, res) {
+//   res.send("GET / " +res.statusCode+"\n");
+//   //next();
+// });
 /**
 app.use("/", function (req, res, next) {
   res.send('Time:' + Date.now());
@@ -27,18 +49,11 @@ app.get("/", function(req, res){
 })
 **/
 
-app.get("/news", function(req, res){
-	res.send("This is news");
-})
+// app.get("/news", function(req, res){
+// 	res.send("This is news");
+// })
 
 //req.params getting parameters after our slash, must pass as argument before
 //git clone GITHUB URL
-
-app.engine('html', swig.renderFile);
-app.set('view engine', 'html');
-app.set('views', __dirname+ "/views");
-
-swig.setDefaults({ cache: false});
-
 
 
